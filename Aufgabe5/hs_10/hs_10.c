3b99e12b4ebf039eaa7937043f2c6b4253d3a780
NancySalama <117858061+NancySalama@users.noreply.github.com>
2022-11-09 21:44:59 +0100

    Update hs_10.c

--- Aufgabe5/hs_10/hs_10.c
+++ Aufgabe5/hs_10/hs_10.c
@@ -2,16 +2,13 @@
  * Aufgabe = 5.1*/
 
 #include<stdio.h>
-
 void tausche(int *ptr_1, int *ptr_2);
-
 void tausche(int *ptr_1, int *ptr_2){
 	int temp;
 	temp = *ptr_1;
 	*ptr_1 = *ptr_2;
 	*ptr_2 = temp;
 }
-
 int main(){
 	int a = 2;
        	int b = 3;
@@ -23,3 +20,20 @@ int main(){
 	printf("\na = %d und b = %d\n", a, b);
 	return(0);	
 }
+#include<stdio.h>
+int main (){
+	int var1 = 5;
+	int var2 = 7;
+	printf("var1 before : %d\n",var1);
+	printf("var2 before : %d\n",var2);
+	swap(&var1, &var2);
+	printf("var1 after : %d\n",var1);
+	printf("var2 after : %d\n",var2);
+	return 0;
+}
+void swap(int* p1, int* p2){
+	int a = *p1;
+	*p1 = *p2;
+	*p2 = a;
+}
+
